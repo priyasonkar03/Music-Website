ec667e59f0360b579959c6641d1b0350168bdbc5
Priya Sonkar <priyasonkar0003@gmail.com>
2024-07-26 12:59:45 +0530

    Solving Navbar Challanges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-   <h1 className="text-2xl text-center" >Hello NextJS</h1>
-  );
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+     <h1 className="text-2xl text-center" >Hello NextJS</h1>
+   </main>
+    );
 }
