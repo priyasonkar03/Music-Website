e4320023b8b9a3c0c07220b658e28407172c5ffc
Priya Sonkar <priyasonkar0003@gmail.com>
2024-07-26 19:00:30 +0530

    edit the view button

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -70,7 +70,7 @@ function UpcomingWebinars() {
 
       <div className="mt-10 text-center">
         <Link href={"/"}
-        className="px-4 py-2 rounded border border-neutral-600 text-neutral-700 bg-white hover:bg-gray-100 transition duration-200"
+        className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
         >
           View All webinars
         </Link>
