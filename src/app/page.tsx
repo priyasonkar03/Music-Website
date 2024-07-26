16bee54b8305569c1df2859de576d5b215421b5d
Priya Sonkar <priyasonkar0003@gmail.com>
2024-07-26 18:08:41 +0530

    add courses and contact pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructor from "@/components/Instructor";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
@@ -15,6 +16,7 @@ export default function Home() {
      <MusicSchoolTestimonials/>
      <UpcomingWebinars/>
      <Instructor/>
+     <Footer/>
    </main>
     );
 }
